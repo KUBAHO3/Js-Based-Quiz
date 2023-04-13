5173f053957ae64e62744e323233eb41e5606fae
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-13 23:30:06 +0530

    Add gues answer js

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function Quiz(questions) {
     this.questions = questions;
     this.currentQuestionIndex = 0;
 }
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+    this.currentQuestionIndex++;
+};
+
+Quiz.prototype.getCurrentQuestion = function() {
+    return this.questions[this.currentQuestionIndex];
+};
+
+Quiz.prototype.hasEnded = function() {
+    return this.currentQuestionIndex >= this.questions.length;
+};
