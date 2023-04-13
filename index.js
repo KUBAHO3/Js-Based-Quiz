4cad6193814029b24bdb7b81dcad88e8a266c580
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-13 23:32:42 +0530

    Add score section js

--- index.js
+++ index.js
@@ -40,6 +40,35 @@ var QuizUI = {
     displayQuestion: function() {
         this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
     },
+    displayChoices: function() {
+        var choices = quiz.getCurrentQuestion().choices;
+
+        for(var i = 0; i < choices.length; i++) {
+            this.populateIdWithHTML("choice" + i, choices[i]);
+            this.guessHandler("guess" + i, choices[i]);
+        }
+    },
+    displayScore: function() {
+        var gameOverHTML = "<h1>Game Over</h1>";
+        gameOverHTML += "<h2> Your score is: " + quiz.score + "</h2>";
+        this.populateIdWithHTML("quiz", gameOverHTML);
+    },
+    
+    populateIdWithHTML: function(id, text) {
+        var element = document.getElementById(id);
+        element.innerHTML = text;
+    },
+    guessHandler: function(id, guess) {
+        var button = document.getElementById(id);
+        button.onclick = function() {
+            quiz.guess(guess);
+            QuizUI.displayNext();
+        }
+    },
+    
 }
+//Create Quiz
+var quiz = new Quiz(questions);
+
 //Display Quiz
 QuizUI.displayNext();
