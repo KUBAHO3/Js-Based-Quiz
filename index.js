100eb3329ab98f796898e5dc3777b2068bdb7eb7
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-13 23:31:31 +0530

    Add question section js

--- index.js
+++ index.js
@@ -18,3 +18,28 @@ Quiz.prototype.getCurrentQuestion = function() {
 Quiz.prototype.hasEnded = function() {
     return this.currentQuestionIndex >= this.questions.length;
 };
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+};
+var QuizUI = {
+    displayNext: function () {
+        if (quiz.hasEnded()) {
+            this.displayScore();
+        } else {
+            this.displayQuestion();
+            this.displayChoices();
+            this.displayProgress();
+        }
+    },
+    displayQuestion: function() {
+        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    },
+}
+//Display Quiz
+QuizUI.displayNext();
